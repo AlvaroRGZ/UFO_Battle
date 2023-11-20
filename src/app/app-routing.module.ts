95251b0e721b37ccf9153528e049669f65d764e5
Alvaro Rodriguez Gomez <alu0101362953@ull.edu.es>
2023-11-20 20:12:58 +0100

    Added basic components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PresentationComponent} from "./presentation/presentation.component";
+import {RegistrationComponent} from "./registration/registration.component";
+import {PreferencesComponent} from "./preferences/preferences.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'presentation', component: PresentationComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: 'preferences', component: PreferencesComponent},
+  {path: ' ', redirectTo: 'presentation', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RegistrationComponent } from './registration/registration.component';
+import { PreferencesComponent } from './preferences/preferences.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    PresentationComponent,
+    RegistrationComponent,
+    PreferencesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
