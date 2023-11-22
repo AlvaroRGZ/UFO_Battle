df4fc36c6dea73c6e0bd85526ba2422e3e2a7224
Alvaro Rodriguez Gomez <alu0101362953@ull.edu.es>
2023-11-22 13:22:01 +0100

    Added toastr dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +26,9 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
     HttpClientModule,
     FormsModule,
     BrowserModule,
